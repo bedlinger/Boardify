e6dff695f9a38fcdb31de89bdac9753f8e428a34
Benjamin Edlinger <beni.edlinger@gmail.com>
2025-04-12 22:52:12 +0200

    format and cleanup

--- Boardify/Models/Ticket.cs
+++ Boardify/Models/Ticket.cs
@@ -1,49 +1,45 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace Boardify.Models
+namespace Boardify.Models;
+
+public class Ticket
 {
-    public class Ticket
-    {
-        [JsonPropertyName("id")]
-        required public string Id { get; set; }
-        [JsonPropertyName("stage_nr")]
-        public int StageNr { get; set; }
-        [JsonPropertyName("title")]
-        required public string Title { get; set; }
-        [JsonPropertyName("description")]
-        required public string Description { get; set; }
-        [JsonPropertyName("created_at")]
-        public DateTime CreatedAt { get; set; }
-        [JsonPropertyName("due_at")]
-        public DateTime? DueAt { get; set; }
-        [JsonPropertyName("is_done")]
-        public bool IsDone { get; set; }
+    [JsonPropertyName("id")] public required string Id { get; set; }
 
-        public string CreatedDateFormatted => CreatedAt.ToString("dd.MM.yyyy");
-        public string? DueDateFormatted => DueAt?.ToString("dd.MM.yyyy");
-    }
+    [JsonPropertyName("stage_nr")] public int StageNr { get; set; }
 
-    public class TicketCreate
-    {
-        [JsonPropertyName("stage_nr")]
-        public int StageNr { get; set; }
-        [JsonPropertyName("title")]
-        required public string Title { get; set; }
-        [JsonPropertyName("description")]
-        required public string Description { get; set; }
-        [JsonPropertyName("due_at")]
-        public DateTime? DueAt { get; set; }
-    }
+    [JsonPropertyName("title")] public required string Title { get; set; }
 
-    public class TicketUpdate
-    {
-        [JsonPropertyName("stage_nr")]
-        public int? StageNr { get; set; }
-        [JsonPropertyName("title")]
-        public string? Title { get; set; }
-        [JsonPropertyName("description")]
-        public string? Description { get; set; }
-        [JsonPropertyName("due_at")]
-        public DateTime? DueAt { get; set; }
-    }
+    [JsonPropertyName("description")] public required string Description { get; set; }
+
+    [JsonPropertyName("created_at")] public DateTime CreatedAt { get; set; }
+
+    [JsonPropertyName("due_at")] public DateTime? DueAt { get; set; }
+
+    [JsonPropertyName("is_done")] public bool IsDone { get; set; }
+
+    public string CreatedDateFormatted => CreatedAt.ToString("dd.MM.yyyy");
+    public string? DueDateFormatted => DueAt?.ToString("dd.MM.yyyy");
+}
+
+public class TicketCreate
+{
+    [JsonPropertyName("stage_nr")] public int StageNr { get; set; }
+
+    [JsonPropertyName("title")] public required string Title { get; set; }
+
+    [JsonPropertyName("description")] public required string Description { get; set; }
+
+    [JsonPropertyName("due_at")] public DateTime? DueAt { get; set; }
+}
+
+public class TicketUpdate
+{
+    [JsonPropertyName("stage_nr")] public int? StageNr { get; set; }
+
+    [JsonPropertyName("title")] public string? Title { get; set; }
+
+    [JsonPropertyName("description")] public string? Description { get; set; }
+
+    [JsonPropertyName("due_at")] public DateTime? DueAt { get; set; }
 }
