0f16b9b1105931484817661d333180b31f53c609
Benjamin Edlinger <beni.edlinger@gmail.com>
2025-04-20 00:49:57 +0200

    add User, UserCredentials, and Token models for authentication

---
+++ Boardify/Models/Auth.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Boardify.Models;
+
+public class User
+{
+    [JsonPropertyName("username")] public required string Username { get; set; }
+
+    [JsonPropertyName("id")] public required string Id { get; set; }
+}
+
+public class UserCredentials
+{
+    [JsonPropertyName("username")] public required string Username { get; set; }
+
+    [JsonPropertyName("password")] public required string Password { get; set; }
+}
+
+public class Token
+{
+    [JsonPropertyName("access_token")] public required string AccessToken { get; set; }
+
+    [JsonPropertyName("token_type")] public required string TokenType { get; set; }
+}
